8ad492aa5f727a3770ca3fe58acf502a4131bc94
livelybone <2631541504@qq.com>
2018-08-29 21:57:40 +0800

    edit

---
+++ src/common/CascaderMixin.js
@@ -0,0 +1,10 @@
+/**
+ * @props {Array} options
+ *    example: [{name: 'a', value:'1', children: [{name:'a1', value:'11', children:[...]}]}]
+ * */
+export default {
+  props: {
+    value: Array,
+    options: Array,
+  },
+}
